e1c9ea94c1211d552f1c86e2f34db1ec42a8302b
Roy <royjairusraj@hotmail.com>
2025-03-29 19:25:01 +0000

    Basic navbar added

--- lib/about/about.dart
+++ lib/about/about.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/shared/bottom_nav.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      appBar: AppBar(title: const Text("About")),
+      bottomNavigationBar:
+          const BottomNavBar(), // Assuming BottomNavBar is defined elsewhere
+      body: const Center(child: Text("This is the About page.")),
+    );
   }
 }
