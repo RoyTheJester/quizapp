e18f7c0ee1748825018cc424ebc9668aa5111469
Roy <royjairusraj@hotmail.com>
2025-05-03 17:17:47 +0100

    Call to render topics from Firebase

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/services/firestore.dart';
+import 'package:quizapp/services/models.dart';
 import 'package:quizapp/shared/bottom_nav.dart';
+import 'package:quizapp/shared/error.dart';
+import 'package:quizapp/shared/loading.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(bottomNavigationBar: BottomNavBar());
+    // We can use the FutureBuilder widget to fetch the topics from the API and display them in a list view based on the topics we get from the API.
+    // We type this so that we can get good intellisense and type checking.
+    return FutureBuilder<List<Topic>>(
+      future: FirestoreService().getTopics(),
+      // These are the 4 states that the topic can be in when fetching data from the API.
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const LoadingScreen();
+        } else if (snapshot.hasError) {
+          return Center(
+            child: ErrorMessage(message: snapshot.error.toString()),
+          );
+        } else if (snapshot.hasData) {
+          var topics = snapshot.data!;
+
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.deepPurple,
+              title: const Text("Topics"),
+            ),
+            // Using count we put 2 items in a row and they rest follow in the next row infinitely
+            body: GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: topics.map((topic) => Text(topic.title)).toList(),
+            ),
+            bottomNavigationBar: const BottomNavBar(),
+          );
+        } else {
+          return const Text("No topics found in Firestore. Check database.");
+        }
+      },
+    );
   }
 }
